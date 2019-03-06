d417b4d28568353c7f86c62b9b69e08db2812588
LeThinh2511 <thinhlefrs@gmail.com>
2019-03-06 19:57:28 +0700

    Data structure [Bag]: Bag implementation

---
+++ DataStructure/Bag/src/implementation/Bag.java
@@ -0,0 +1,66 @@
+package implementation;
+
+import java.util.Iterator;
+
+/**
+ * Author: thinhle
+ * Date: 2019-03-06
+ */
+
+public class Bag<Item> implements Iterable<Item> {
+    private Node first;
+    private int size;
+
+    public Bag() {
+        this.first = null;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void add(Item item) {
+        Node newNode = new Node(item);
+        if (this.isEmpty()) {
+            this.first = newNode;
+            this.size += 1;
+            return;
+        }
+        newNode.next = first;
+        this.first = newNode;
+        this.size += 1;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new Iterator<Item>() {
+            private Node currentNode = first;
+            @Override
+            public boolean hasNext() {
+                return currentNode != null;
+            }
+
+            @Override
+            public Item next() {
+                Node node = currentNode;
+                currentNode = currentNode.next;
+                return (Item) node.item;
+            }
+        };
+    }
+
+    class Node<Item> {
+        Item item;
+        Node next;
+
+        public Node(Item item) {
+            this.item = item;
+            this.next = null;
+        }
+    }
+}
